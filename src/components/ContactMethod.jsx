b7a941eeb3df503eb331337afbb37373047db17d
Sam J <88912351+ssg406@users.noreply.github.com>
2022-10-25 17:23:27 -0700

    mobile contact page complete

--- src/components/ContactMethod.jsx
+++ src/components/ContactMethod.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import DynamicIcon from './DynamicIcon';
 
-const ContactMethod = () => {
+const ContactMethod = ({icon, text, link}) => {
   return (
-    <div>ContactMethod</div>
+    <div className='flex items-center gap-2 my-6'>
+      <a href={link} target='_blank' rel='noopener noreferrer'><DynamicIcon name={icon} iconClass='text-5xl text-blue-800 hover:text-blue-600 transition-colors ease-in-out' /></a>
+      <span className='text-2xl font-poppins font-semibold tracking-tight'><a href={link} target='_blank' rel='noopener noreferrer'>{text}</a></span>
+    </div>
   )
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { contactMethods } from '../data';
+import { ContactMethod } from '../components';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <main className='md:container md:mx-auto p-4'>
+      <h2 className="text-2xl md:text-6xl font-bold font-poppins border-b-4 border-black my-2 md:my-6">Contact Me</h2>
+      <div className='p-2'>
+        {
+          contactMethods.map((contactData) => {
+            return <ContactMethod {...contactData} />
+          })
+        }
+      </div>
+    </main>
   )
 }
 
